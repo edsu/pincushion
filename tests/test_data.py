53608f39eaadd9922dc0a9219ff4e0d9705dcb8d
Ed Summers <ehs@pobox.com>
2024-10-03 08:50:59 -0400

    simplify data test

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,24 +1,11 @@
 import logging
-import shutil
-from pathlib import Path
-
-from pytest import fixture
 
 from pincushion import historypin
 
 logging.basicConfig(filename="test.log", level=logging.INFO)
 
 
-@fixture
-def archive_dir():
-    d = Path("./test-archive")
-    if d.is_dir():
-        shutil.rmtree(d)
-    d.mkdir()
-    yield d
-
-
-def test_data(archive_dir):
+def test_data():
     data = historypin.get_data(user_id=120327)
 
     assert "user" in data
